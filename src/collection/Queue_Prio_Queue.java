36679ab7ae571f734d22c4214f83d75e28ca2155
Aditya <adipandit8891@gmail.com>
2021-12-21 19:10:40 +0530

    GitProject

---
+++ src/collection/Queue_Prio_Queue.java
@@ -0,0 +1,25 @@
+package collection;
+
+import java.util.PriorityQueue;
+
+public class Queue_Prio_Queue {
+
+	public static void main(String[] args) {
+		PriorityQueue pq =new PriorityQueue<>();
+		
+		pq.add("Pune");
+		pq.add("Call");
+		pq.add("Sia");
+		pq.offer("Velo");                     //add element to queue
+		pq.offer("kavya");
+		System.out.println(pq);
+		
+		//System.out.println(pq.peek());      //fetch first elemnt
+		//System.out.println(pq.element());    //fetch first record
+		System.out.println(pq.poll());       //delte first elmnt
+		
+		System.out.println(pq);
+
+	}
+
+}
